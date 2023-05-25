7de1537f0c38e6364d9044cc8f350c075b0c4018
Addison <72429964+addipls@users.noreply.github.com>
2023-05-25 00:39:14 -0700

    Connect to wifi + turn on LED while connecting

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "led.h"
 #include <WiFi.h>
 
+const char* ssid = "";
+const char* password = "";
+
 // Define the GPIO pin for the onboard LED
 LED led;
 int counter = 0;
@@ -9,6 +12,14 @@ int counter = 0;
 void setup() {
   pinMode(LED_PIN, OUTPUT); // Initialize the LED pin as an output
   led.init();
+  led.update(true);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+
+  }
 }
 
 void loop() {
@@ -22,7 +33,7 @@ void loop() {
     led.update(false);
     counter = 0;
   }
-
+  Serial.println("Connected to the WiFi network");
   led.update();
   delay(1000);
 }
